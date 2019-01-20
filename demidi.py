703700858c7bd8d8fe64756fe6623686dfde8958
Stephanie Wagner <stephwag@stephwag.com>
2019-01-19 22:53:58 -0500

    init commit

---
+++ demidi.py
@@ -0,0 +1,141 @@
+import argparse
+import midi
+from midi import *
+import os
+import json
+import sys
+import json
+
+def gen_vocab(tick_max, include_resolutions = False, resolutions = False):
+    events = [i for i in dir(midi.events) if i.find('Event') >= 0]
+    vocab = {}
+    count = 0
+
+    # tracks
+    for i in range(16):
+        for e in events:
+            vocab[str(i) + e] = count
+            count += 1
+
+    for i in range(tick_max + 1):
+        vocab[str(i) + 't'] = count
+        count += 1
+
+    # channels
+    for i in range(128):
+        vocab[str(i) + 'c'] = count
+        count += 1
+
+    # data array
+    for i in range(128):
+        vocab[str(i) + 'd'] = count
+        count += 1
+
+    # resolutions
+    if include_resolutions:
+        for r in resolutions:
+            vocab[str(r) + 'r'] = count
+            count += 1
+
+    with open('vocab.json', 'w') as f:
+        f.write(json.dumps(vocab))
+
+    print("Generated vocab.json in {}".format(os.path.dirname(os.path.realpath('vocab.json'))))
+
+def demidi(midi_dir, data_dir, out_dir, include_resolutions, tick_max, generate_vocab):
+    midifiles = []
+    for root, dirs, filenames in os.walk(midi_dir):
+        for f in filenames:
+            if f.endswith(".mid"):
+                midifiles.append(f)
+
+    # 96 is quarter notes, but if giving music with variing notes, do not set this.
+    masterresolution = 96
+    # mother 3 music is 24 (and highest tick is only 2489)
+    # undertale/deltarune is 96
+
+    biggest_tick = 0
+    fff = ''
+    goodticks = []
+    badsongs = []
+    resolutions = []
+    events_blacklist = ['CopyrightMetaEvent', 'AbstractEvent', 'MetaEvent', 'MarkerEvent', 'SomethingEvent', 'CuePointEvent']
+
+    for midifile in midifiles:
+        try:
+            pattern = midi.read_midifile("{}/{}".format(midi_dir, midifile))
+        except:
+            continue
+
+        resolutions.append(pattern.resolution)
+        count = 0
+        words = []
+        word = ""
+        write_file = True
+        for p in pattern:
+            for e in p:
+                if e.__class__.__name__ not in events_blacklist:
+                    word = ""
+                    word += str(count) + e.__class__.__name__.lower()
+                    word += str(e.tick) + 't'
+
+                    if tick_max >= 0:
+                        if e.tick >= tick_max:
+                            badsongs.append(midifile)
+                        else:
+                            goodticks.append(e.tick)
+
+                        if e.tick > biggest_tick:
+                            biggest_tick = e.tick
+
+                    if e.__class__.__name__ not in ['InstrumentNameEvent', 'MarkerEvent', 'TrackNameEvent', 'TimeSignatureEvent', 'SetTempoEvent', 'EndOfTrackEvent', 'CopyrightMetaEvent', 'KeySignatureEvent']:
+                        word += str(e.channel) + 'c'
+                    else:
+                        print(e)
+
+                    for d in e.data:
+                        word += str(d) + 'd'
+                    words.append(word)
+            count += 1
+
+        if midifile not in badsongs or tick_max < 0:
+            with open("{}/{}.txt".format(data_dir, midifile), 'w') as f:
+                if include_resolutions:
+                    f.write("{} {}".format(pattern.resolution, " ".join(words)))
+                else:
+                    f.write(" ".join(words))
+
+        resolutions = list(set(resolutions))
+
+        print("Largest tick found: {}\nResolutions found: {}".format(biggest_tick, resolutions))
+
+        if generate_vocab:
+            gen_vocab(biggest_tick, include_resolutions=include_resolutions, resolutions=resolutions)
+
+def main(args):
+    if args.midi_dir is None or args.data_dir is None or args.out_dir is None:
+        parser.print_help()
+        return 0
+
+    demidi(args.midi_dir, args.data_dir, args.out_dir, args.include_resolutions, args.tick_max, args.generate_vocab)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Flatten midi files and generate vocab for training neural networks. More info at https://github.com/stephwag/midi-rnn')
+    parser.add_argument('--mididir', metavar='-M', dest='midi_dir', default=None,
+                       help='Absolute path to data directory')
+    parser.add_argument('--datadir', metavar='-D', dest='data_dir', default=None,
+                       help='Absolute path to data directory')
+
+    parser.add_argument('--outdir', metavar='-O', dest='out_dir', default=None, help='Absolute path to output directory')
+    parser.add_argument('--include-resolutions', action='store_true', dest='include_resolutions', help='Midi resolution of the out file (default: 96)')
+    parser.add_argument('--vocab', dest='generate_vocab', action='store_true', help='Generate vocab (default: false)')
+
+    parser.add_argument('--tickmax', metavar='-T', dest='tick_max', nargs=1, default=-1, type=int,
+                       help='Only process midis that are less or equal to this value. The lower the value, the lower the size of the vocab (default: no max)')
+
+    args = parser.parse_args()
+    main(args)
+    
+
+
+
---
+++ remidi.py
@@ -0,0 +1,117 @@
+import argparse
+import midi
+from midi import *
+import os
+from random import randint
+import json
+import sys
+
+
+
+def get_pair(text):
+    number = int(''.join(filter(str.isdigit, text)))
+    l = len(str(number))
+    return (number, text[l:])
+
+def remidi(data_file, out_file, resolution=96):
+    events_list = [i for i in dir(midi.events) if i.find('Event') >= 0]
+    events_hash = {}
+    for evt in events_list:
+        events_hash[evt.lower()] = evt
+
+    with open(data_file, 'r') as f:
+        textdata = f.read().split(" ")
+
+    pattern = midi.Pattern()
+
+    pattern.resolution = resolution
+
+    tracks = {}
+
+    for index in range(0, len(textdata)):
+
+        t = textdata[index]
+        result = t
+
+        params = {}
+
+        tick = None
+        channel = None
+        data = []
+
+        i = t.find('event') + 5
+
+        etext = t[:i]
+        ev_num, ev = get_pair(etext)
+        print(ev)
+        c = getattr(midi, events_hash[ev])
+        t = t[i:]
+
+        params["track"] = ev_num
+        params["event"] = ev
+        params["class"] = c
+
+        if t.find('t') >= 0 and len(t) > 0:
+            ttext = t[:t.find('t')]
+            if len(ttext) > 0:
+                params['tick'] = get_pair(ttext)[0]
+                t = t[t.find('t'):]
+
+        if t.find('c') >= 0 and len(t) > 0:
+            ctext = t[:t.find('c')]
+            if len(ctext) > 0:
+                params['channel'] = get_pair(ctext)[0]
+                t = t[t.find('c'):]
+
+        if t.find('d') >= 0 and len(t) > 0:
+            params['data'] = []
+            ds = t.split('d')
+            for dd in ds:
+                if len(dd) > 0:
+                    params['data'].append(get_pair(dd)[0])
+
+        if ev_num not in tracks:
+            tracks[ev_num] = [ params ]
+        else:
+            tracks[ev_num].append(params)
+
+    keys = sorted(tracks.keys())
+    for k in keys:
+        trk = midi.Track()
+        tname_param = None
+        for pm in tracks[k]:
+            if pm['class'].__name__ == 'TrackNameEvent':
+                klass = pm['class']
+                trk.append(TrackNameEvent(tick=pm["tick"], text='TRACK ' + str(k), data=pm["data"]))
+
+        for pm in tracks[k]:
+            if pm['class'].__name__ != 'TrackNameEvent' and pm['class'].__name__ != 'CopyrightMetaEvent':
+                klass = pm['class']
+                trk.append(klass(**pm))
+        pattern.append(trk)
+    print(pattern)
+
+    midi.write_midifile(out_file, pattern)
+
+def main(args):
+    if args.data_file is None or args.out_file is None:
+        parser.print_help()
+        return 0
+
+    remidi(args.data_file, args.out_file, args.resolution)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create midi files from data that use demidi.py syntax. More info at https://github.com/stephwag/midi-rnn')
+    parser.add_argument('--datafile', metavar='-M', dest='data_file', default=None,
+                       help='Path to a single data file (must be an absolute path)')
+
+    parser.add_argument('--outfile', metavar='-O', dest='out_file', default=None, help='Output file path (must be an absolute path)')
+    parser.add_argument('--resolution', metavar='-R', dest='resolution', default=96, type=int, help='Midi resolution of the out file (default: 96)')
+
+    args = parser.parse_args()
+    main(args)
+    
+
+
+
